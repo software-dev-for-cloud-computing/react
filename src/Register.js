9b318207d1d36aad95f258f874ccb6ea3ab55b43
Julian <js437@hdm-stuttgart.de>
2024-08-16 12:45:19 +0200

    update env 6

--- src/Register.js
+++ src/Register.js
@@ -16,7 +16,7 @@ function Register({ onRegister, onToggle }) {
     }
 
     try {
-        const response = await fetch(`http://localhost:3000/api/auth/register`, {
+        const response = await fetch(`/api/auth/register`, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
