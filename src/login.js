ed64aefe8cf30f8888651e4550aa53b1a6ea32f3
Julian <js437@hdm-stuttgart.de>
2024-08-16 08:36:04 +0200

    test frontend

--- src/login.js
+++ src/login.js
@@ -9,7 +9,7 @@ function Login({ onLogin, onToggle }) {
     event.preventDefault();
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
